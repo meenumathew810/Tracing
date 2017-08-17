2530a48b1507256a0340e371f2bfc7f752b45412
mmathew <mmathew@SUYPC194>
2017-08-17 13:41:52 +0530

    Add project files.

---
+++ Tracing/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tracing.Startup))]
+namespace Tracing
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Tracing/TracingWebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tracing
+{
+    public partial class TracingWebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void CreatePerson()
+        {
+            List<Person> person = new List<Person>();
+            person.Add(new Person
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            });
+            Session["Person"] = person;
+            if (Trace.IsEnabled)
+                Trace.Write("Session Created");
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CreatePerson();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var person = ((List<Person>)Session["Person"]);
+            try
+            {
+                person.RemoveAt(0);
+                Trace.Write("deleted");
+            }
+            catch (Exception ex)
+            {
+                Trace.Warn(ex.Message);
+            }
+            Session["Person"] = person;
+
+        }
+    }
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Concat(FirstName, LastName);
+            }
+            set
+            {
+                FullName = string.Concat(FirstName, LastName);
+            }
+        }
+
+    }
+}
